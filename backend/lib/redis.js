ae13f259dd715a5a290c4f998fd6304d7d31df33
lami216 <iiyad21694@gmail.com>
2025-10-19 00:53:13 +0000

    Ensure backend loads env from root (#25)

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -1,6 +1,7 @@
-import Redis from "ioredis";
 import dotenv from "dotenv";
+import path from "path";
+import Redis from "ioredis";
 
-dotenv.config();
+dotenv.config({ path: path.resolve("./backend/.env") });
 
 export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
